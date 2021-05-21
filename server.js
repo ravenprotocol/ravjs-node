20d17df253957e02f2ecdcea2fd71de6f3873863
Steven Yuan <syuan100@gmail.com>
2021-05-21 15:53:27 -0700

    implement op event

--- server.js
+++ server.js
@@ -8,6 +8,23 @@ io.of("/ravjs").on('connection', function (socket) {
     socket.on('CH01', function (from, msg) {
         console.log('Log', from, ' saying ', msg);
     });
+
+    socket.on('op', function (d) {
+        var data = JSON.parse(d);
+
+        //Acknowledge op
+        socket.emit("acknowledge", JSON.stringify({
+            "op_id": data.op_id,
+            "message": "Op received"
+        }));
+
+        // Perform
+        let operation_type = data ["op_type"];
+        let operator = data ["operator"];
+        if (operation_type && operator) {
+            compute(data);
+        }
+    });
 });
 
 http.listen(9999, function () {
